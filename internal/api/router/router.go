b99789566b0aa99fe727782cc71dfcfca338d1fc
Pedro Rodriguez <pedro00627@gmail.com>
2025-12-22 04:25:27 -0500

    adding cors

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pedro00627/image-analyzer-api/internal/api/handler"
 	"github.com/pedro00627/image-analyzer-api/internal/application/service"
@@ -9,6 +12,16 @@ import (
 
 // SetupRoutes configures all API routes
 func SetupRoutes(engine *gin.Engine, container bootstrap.DependencyContainer) {
+	// Configure CORS
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://pedro00627.com", "https://www.pedro00627.com", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	// Get use case from container
 	useCase := container.GetAnalyzeImageUseCase()
 
